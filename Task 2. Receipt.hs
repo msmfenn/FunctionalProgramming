723d79dc9a9262b4c88b3a970fcf3fcf203f00f3
peshopbs2 <peshopbs2@gmail.com>
2022-01-03 21:04:47 +0200

    Update Task 2. Receipt.hs

--- Task 2. Receipt.hs
+++ Task 2. Receipt.hs
@@ -1,26 +1,24 @@
- -- receipt
- calculateVAT x = x*1.2
+-- receipt
+calculateVAT x = x * 1.2
 
- main = do
-   input1 <- getLine
-   let quantities = read input1 :: [Float]
-  
-   input2 <- getLine
-   let prices = read input2 :: [Float]
-   
-   let pricesWithVAT  = map calculateVAT prices
-   let pricesWithVAT1  = map (\x->x*1.2) prices -- alternatively
- 
-   let values = zipWith (*) quantities prices
-   let totalSum = foldl (+) 0 values
+main = do
+  input1 <- getLine
+  let quantities = read input1 :: [Float]
 
-   let valuesWithVAT = zipWith (*) quantities pricesWithVAT
-   let totalSumWithVAT = foldl (+) 0 valuesWithVAT
- 
-   print(values)
-   print(totalSum)
-   print(totalSum*1.2)
+  input2 <- getLine
+  let prices = read input2 :: [Float]
 
-   print(valuesWithVAT)
-   print(totalSumWithVAT)
- 
+  let pricesWithVAT = map calculateVAT prices
+  let pricesWithVAT1 = map (* 1.2) prices -- alternatively
+  let values = zipWith (*) quantities prices
+  let totalSum = sum values
+
+  let valuesWithVAT = zipWith (*) quantities pricesWithVAT
+  let totalSumWithVAT = sum valuesWithVAT
+
+  print values
+  print totalSum
+  print (totalSum * 1.2) -- the total sum with the VAT coefficient
+
+  print valuesWithVAT
+  print totalSumWithVAT --the total sum with VAT obtained with summation, should be the same as the totalSum * 1.2 if everything is correct
