ecbf224a7c219336d70bf9234ec03f32c24e15e5
peshopbs2 <peshopbs2@gmail.com>
2022-01-03 20:59:17 +0200

    Update Task 1. Sum Of Elements.hs

--- Task 1. Sum Of Elements.hs
+++ Task 1. Sum Of Elements.hs
@@ -1,21 +1,22 @@
- sumElements totalSum list  = 
-     if null list
-     then totalSum
-     else sumElements (totalSum + (head list)) (tail list)  
- sumElements1 totalSum [] = totalSum
- sumElements1 totalSum (x:xs) = sumElements1 (x + totalSum) xs
+sumElements totalSum list =
+  if null list
+    then totalSum
+    else sumElements (totalSum + head list) (tail list)
 
- sumElements2 list= foldl (+) 0 list
+sumElements1 totalSum [] = totalSum
+sumElements1 totalSum (x : xs) = sumElements1 (x + totalSum) xs
 
- main = do
+sumElements2 = foldl (+) 0
 
+main = do
   input <- getLine
-  let list = read input::[Integer]
-  
+  let list = read input :: [Integer]
+
   let result = sumElements 0 list
-  print(result)
-  
+  print result
+
   let result1 = sumElements1 0 list
-  print(result1)
+  print result1
+
   let result2 = sumElements2 list
-  print(result2)
+  print result2
